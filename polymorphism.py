0d70721af1b3cad6dbada51b3730e30eefa386ce
dileepyadavns <102239765+dileepyadavns@users.noreply.github.com>
2022-04-15 21:17:37 +0530

    Update polymorphism.py

--- polymorphism.py
+++ polymorphism.py
@@ -1,10 +1,5 @@
 
-
-
-
-
-# **Polymorphisam**
-
+# Polymorphisam
 
 class Cat():
   def __init__(self,name):
@@ -12,28 +7,17 @@ class Cat():
   def speak(self):
    return self.name + " says Meow!"   
 
-
-# In[87]:
-
-
 class Dog():
   def __init__(self,name):
     self.name=name
   def speak(self):
    return self.name + " says woof!"   
 
-
-
 my_dog=Dog("niko")
 my_cat=Cat("felix")
 
-
-
-
 for pet in (my_dog,my_cat):
   print(pet.speak())
-
-
 
 class Animal():
   def __init__(self,name):
@@ -41,11 +25,8 @@ class Animal():
   def speak(self):
     raise NotImplementedError("subclass must implememt this abstract method")  
 
-
-
 myanimal=Animal('fred')
 # myanimal.speak() #Abstract Method this cant we  implemented from parent class
-
 
 class Cat():
   def __init__(self,name):
@@ -53,14 +34,8 @@ class Cat():
   def speak(self):
     return self.name +" can say Meow!"
 
-
-
-
 mycat=Cat("felix")
 print(mycat.speak())
-
-
-
 
 class Dog():
   def __init__(self,name):
@@ -68,14 +43,11 @@ class Dog():
   def speak(self):
     return self.name +" can say woooh!"
 
-
-
-
 mydog=Dog("fred")
 print(mydog.speak())
 
 
-# **Magic methods**
+# Magic methods
 
 class Dog():
   def __init__(self,name):
@@ -86,11 +58,6 @@ class Dog():
   def __str__(self):
     return "string is appeared"  
 
-
-
 c=Dog("fred")
-
 print(c)
-
-
-c.__str__()
+c.__str__() #magic method
