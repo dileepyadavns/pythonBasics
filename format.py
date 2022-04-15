6291a066a05b375485c33d69fe2e81108a9f1c20
dileepyadavns <102239765+dileepyadavns@users.noreply.github.com>
2022-04-15 20:21:34 +0530

    Add files via upload

---
+++ format.py
@@ -0,0 +1,47 @@
+
+character=input()
+a=int(input())
+b=int(input())
+if character =="+":
+    print(a + b)
+elif character =="-":
+   print(a - b)
+elif character =="*":
+    print(a * b)
+elif character =="/":
+    print(a / b)
+elif character =="%":
+    print(a % b)    
+
+
+print("hello \t wo")
+
+mystring="helllo"
+print(mystring)
+print(mystring[-4])
+
+x='hello dileep'
+x.upper()
+print(x)
+
+print(x.split())
+print(x.split("e"))
+
+y='hi dileep {} what are u {}'
+
+print(y.format('golla','doing'))
+
+print('i am {} suryapet. {} is a {} of Telangana'.format('district','from','which'))
+
+print('i am {1} suryapet. {2} is a {0} of Telangana'.format('district','from','which'))
+
+
+# float formatting follows
+
+result=1010.289999992339
+print('{:.2f}'.format(result))
+
+
+#n='dileep'
+#print(f'hello {n} how are you')
+
