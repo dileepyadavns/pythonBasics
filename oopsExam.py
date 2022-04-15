6145c60f9129bb6a6271da68861b63dfdde1fef0
dileepyadavns <102239765+dileepyadavns@users.noreply.github.com>
2022-04-15 21:14:57 +0530

    Update oopsExam.py

--- oopsExam.py
+++ oopsExam.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# # Object Oriented Programming
-
+# Object Oriented Programming
 
 class Line:
     
@@ -20,45 +17,24 @@ class Line:
         x2,y2=self.coor2
         return (y2-y1)/(x2-x1)
 
-
-
-
 coordinate1 = (3,2)
 coordinate2 = (8,10)
-
-li = Line(coordinate1,coordinate2)
-
-
-
+li = Line(coordinate1,coordinate2) #object 
 print(li.distance())
-
-
-
 print(li.slope())
 
-
-
-
-
 class Cylinder:
-    
     def __init__(self,height=1,radius=1):
         self.height=height
         self.radius=radius
         
     def volume(self):
-       
-        
-        return 3.14*self.radius*self.radius*self.height
+       return 3.14*self.radius*self.radius*self.height
     
     def surface_area(self):
         return 2*3.14*self.radius*(self.radius + self.height)
 
-
-c = Cylinder(2,3)
-
-
-print(round(c.volume(),3))
-
-print(c.surface_area())
+c = Cylinder(2,3) #object
+print(round(c.volume(),3)) #will find volume and will round the digits
+print(c.surface_area()) #to find the surface are 
 
